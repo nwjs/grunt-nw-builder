279ac644bc0b9514cce51eaf79ded0c96a6d054c
Adam Lynch <contact@adamlynch.ie>
2016-07-02 20:23:28 +0100

    Supporting newer NW.js versions

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     nwjs: {
       options: {
-        version: '0.10.0',
+        version: '0.14.6',
         buildDir: './build', // Where the build version of my NW.js app is saved
         credits: './public/Credits.html',
         macIcns: './icon.icns', // Path to the Mac icon file
-        platforms: ['osx', 'win'] // These are the platforms that we want to build
+        platforms: ['osx64', 'win32'] // These are the platforms that we want to build
       },
       src: './public/**/*' // Your NW.js app
     },
@@ -15,5 +15,4 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-nw-builder');
   grunt.registerTask('default', ['nwjs']);
-
 };
